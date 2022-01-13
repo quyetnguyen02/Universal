2fe29c968a90f268ac5a0632dcbb9ca1806a784a
quyet nguyen van <7784738-quyetnguyen02@users.noreply.gitlab.com>
2022-01-13 15:19:54 +0700

    Add project files.

---
+++ Hello world/Entity/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_world.Entity
+{
+    class Account
+    {
+        public int Id { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+
+        public int Status { get; set; }
+    }
+}
